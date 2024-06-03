8a5267a3e7654695606b6369c6c43b0446d29263
kimpm78 <kimsoosang12@gmail.com>
2024-06-03 14:53:17 +0900

    logo-fix&userSeederAdd

--- routes/web.php
+++ routes/web.php
@@ -5,17 +5,9 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use App\Http\Controllers\InertiaTestController;
+use App\Http\Controllers\ItemController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::resource('items', ItemController::class)->middleware(['auth', 'verified']);
 
 Route::get(
     '/inertia-test',
@@ -41,6 +33,7 @@ Route::get('/inertia/create', [InertiaTestController::class, 'create'])->name('i
 Route::get('/component-test', function () {
     return Inertia::render('ComponentTest');
 });
+
 // 削除ルート
 Route::delete('/inertia/{id}', [InertiaTestController::class, 'delete'])->name('inertia.delete');
 
